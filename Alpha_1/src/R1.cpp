bd3ec4e9c2c4147ec6b0383c07576c88de128a3d
kkan059 <kkan059@aucklanduni.ac.nz>
2013-08-15 04:08:20 -0700

    indentation

--- Alpha_1/src/R1.cpp
+++ Alpha_1/src/R1.cpp
@@ -17,11 +17,15 @@ geometry_msgs::Twist RobotNode_cmdvel;
 bool showDebug = false;
 
 bool makeMovement = true;
+
 #define sampleNumber 10 // represents number of samples in world file.
+
+
 void StageOdom_callback(nav_msgs::Odometry msg)
 {
 
 }
+
 
 void StageLaser_callback(sensor_msgs::LaserScan msg)
 {
@@ -90,38 +94,39 @@ void StageLaser_callback(sensor_msgs::LaserScan msg)
       }
     }	
     if(showDebug)ROS_INFO("-------------------------- END -----------------------------");
-}
+} //ends StageLaser_callback()
 
 
 
 int main(int argc, char** argv){
-std::stringstream rName;
-rName.str("");
-rName << "RobotNode" << argv[1];	
-//You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
-ros::init(argc, argv, rName.str());
-  ros::NodeHandle n;
+
+    std::stringstream rName;
+    rName.str("");
+    rName << "RobotNode" << argv[1];	
+    //You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
+    ros::init(argc, argv, rName.str());
+    ros::NodeHandle n;
 
   
-rName.str("");
-rName << "robot_" << argv[1]<<"/cmd_vel";
-  RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>(rName.str(),1000); 
+    rName.str("");
+    rName << "robot_" << argv[1]<<"/cmd_vel";
+    RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>(rName.str(),1000); 
 
-rName.str("");
-rName << "robot_" << argv[1]<<"/base_scan";
-ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>(rName.str(),1000,StageLaser_callback);
+    rName.str("");
+    rName << "robot_" << argv[1]<<"/base_scan";
+    ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>(rName.str(),1000,StageLaser_callback);
 
-rName.str("");
-rName << "robot_" << argv[1]<<"/odom";
-ros::Subscriber StageOdom_sub = n.subscribe<nav_msgs::Odometry>(rName.str(),1000,StageOdom_callback);
+    rName.str("");
+    rName << "robot_" << argv[1]<<"/odom";
+    ros::Subscriber StageOdom_sub = n.subscribe<nav_msgs::Odometry>(rName.str(),1000,StageOdom_callback);
 
+    int count = 0;
+    ros::Rate r(10); // 10 cycles per second
 
-  int count = 0;
-  ros::Rate r(10); // 10 cycles per second
-  while(n.ok()){
+    while(n.ok()){
+        ++count;
+        ros::spinOnce();
+        r.sleep();
+    }//ends while()
 
-    ++count;
-    ros::spinOnce();
-    r.sleep();
-  }
-}
+}//ends main()
