eaf23478770fe2f93538f862171b8bc4a37f8a29
santhan-k <skus887@aucklanduni.ac.nz>
2013-08-17 20:52:12 -0700

    j

--- src/alpha_two/world/worldCreator_assistant.sh
+++ src/alpha_two/world/worldCreator_assistant.sh
@@ -3,9 +3,9 @@ source ../../../devel/setup.bash
 echo $1
 gnome-terminal -x bash worldCreator_assistant3.sh
 
-for (( i=0; i<$1; i++ ))
-do
-	sheepNumber=$((6+$i))
-	echo $sheepNumber		
-	gnome-terminal -x bash worldCreator_assistant2.sh $sheepNumber
-done
+#for (( i=0; i<$1; i++ ))
+#do
+#	sheepNumber=$((6+$i))
+#	echo $sheepNumber		
+#	gnome-terminal -x bash worldCreator_assistant2.sh $sheepNumber
+#done
--- src/alpha_two/world/worldCreator_assistant2.sh
+++ src/alpha_two/world/worldCreator_assistant2.sh
@@ -1,2 +1,2 @@
 source ../../../devel/setup.bash
-rosrun alpha_two R1 $1
+rosrun alpha_two R1 $1 $2 $3 $4
