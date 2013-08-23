f4155a632cf2d91851ad9dd4a4e9f9f81e95d6eb
kenyeoh <azn_nz@hotmail.com>
2013-08-22 18:31:32 -0700

    Added correct angle reference (goes between 0 - 360)

--- src/alpha_two/src/R1.cpp
+++ src/alpha_two/src/R1.cpp
@@ -18,53 +18,61 @@ geometry_msgs::Twist RobotNode_cmdvel;
 bool showDebug = false; //Show/hide ROS log messages
 vector<float> previous_ranges;
 #define SAMPLE_NUMBER 10 // represents number of samples in world file.
+
+//velocity of the robot
+double linear_x = 1;
+double angular_z = 0;
+
+//pose of the robot
 double px;
 double py;
 double ptheta;
-double theta;
+
 double grassX;
 double grassY;
-double initialPosx;
-double initialPosy;
-double initialTheta;
+double initial_position_x;
+double initial_position_y;
+double initial_theta;
 alpha_two::sheepState newmsg;
 
 void collisionAvoidance(double smallest_range, sensor_msgs::LaserScan msg, int current_lowest_index);
 float CalculateAnglularVelocity();
+
 void StageOdom_callback(nav_msgs::Odometry msg)
 {
-	py = msg.pose.pose.position.x;
-  	px = -msg.pose.pose.position.y;
-  	//ptheta = fmod((2*M_PI) + theta + angles::normalize_angle_positive(asin(msg.pose.pose.orientation.z) * 2), 2*M_PI) * (180/M_PI);
-	theta = msg.pose.pose.orientation.z;
-	theta = floorf(theta * 1000) / 1000;  //Rounding to 3dp
-	//if(theta<0.00){
-	//	theta = 360.00 - fabs(theta*(180.00/PI));
-	//}else{
-	//	theta = theta*(180.00/PI);
-	//}
-	//theta = fmod((2.0*PI) + theta + angles::normalize_angle_positive(asin(msg.pose.pose.orientation.z) * 2.0), 2.0*PI) * (180.0/M_PI);
-	ROS_INFO("INITIAL THETA = %f",initialTheta);
-	ROS_INFO("px: %f	py: %f		theta: %f", px, py, theta);
+  ptheta = fmod((2*M_PI) + initial_theta + angles::normalize_angle_positive(asin(msg.pose.pose.orientation.z) * 2), 2*M_PI) * (180/M_PI);
+  ROS_INFO("Current theta is: %f", ptheta);
+}
+
+void StageBasePose_callback(nav_msgs::Odometry msg)
+{
+  px = msg.pose.pose.position.y;
+  py = -msg.pose.pose.position.x;
+  ROS_INFO("Current x position is: %f", px);
+  ROS_INFO("Current y position is: %f", py);
 }
 
 void StageGrass_callback(alpha_two::grassState msg)
 {
- //ROS_INFO("RECEIVED GRASS MESSAGE FROM: %d",msg.G_ID);
-	if(newmsg.S_State == 1 && newmsg.grass_locked==msg.G_ID && msg.lockedBy != newmsg.S_ID){
-		newmsg.S_State = 0;
-		//ROS_INFO("CHANGED STATE BACK TO LOOKING FOR GRASS");
-	}else if(msg.G_State == 0 && newmsg.S_State == 0){
-		grassX = msg.x;
-		grassY = msg.y;
-		newmsg.S_State=1;
-                newmsg.grass_locked = msg.G_ID;
-		//ROS_INFO("LOCKED GRASS: %d",msg.G_ID);
-	}
-	if(newmsg.grass_locked == msg.G_ID){
-		//newmsg.grass_locked = 0;
-		//ROS_INFO("STILL HAVE GRASS! YAY!");
-	}
+  //ROS_INFO("RECEIVED GRASS MESSAGE FROM: %d",msg.G_ID);
+  if(newmsg.S_State == 1 && newmsg.grass_locked==msg.G_ID && msg.lockedBy != newmsg.S_ID)
+  {
+    newmsg.S_State = 0;
+    //ROS_INFO("CHANGED STATE BACK TO LOOKING FOR GRASS");
+  }
+  else if(msg.G_State == 0 && newmsg.S_State == 0)
+  {
+    grassX = msg.x;
+    grassY = msg.y;
+    newmsg.S_State=1;
+    newmsg.grass_locked = msg.G_ID;
+    //ROS_INFO("LOCKED GRASS: %d",msg.G_ID);
+  }
+  if(newmsg.grass_locked == msg.G_ID)
+  {
+    //newmsg.grass_locked = 0;
+    //ROS_INFO("STILL HAVE GRASS! YAY!");
+  }
 }
 
 
@@ -102,12 +110,9 @@ void StageLaser_callback(sensor_msgs::LaserScan msg)
       smallest_range = msg.ranges[current_lowest_index];
     }
     previous_ranges.push_back(msg.ranges[i]);
-  }  
-  
-  collisionAvoidance(smallest_range, msg, current_lowest_index);
+  }
 
-  // Publish the message
-  RobotNode_stage_pub.publish(RobotNode_cmdvel);
+  collisionAvoidance(smallest_range, msg, current_lowest_index);
 
   if(showDebug)
     ROS_INFO("-------------------------- END -----------------------------");
@@ -115,49 +120,51 @@ void StageLaser_callback(sensor_msgs::LaserScan msg)
 
 // This function prevents robot(sheep) from colliding with stage objects.
 void collisionAvoidance(double smallest_range, sensor_msgs::LaserScan msg, int current_lowest_index){
-  if(newmsg.S_State!=1){  
-	  if(showDebug)
-	    ROS_INFO("Lowest index: %d", current_lowest_index);
+  if(newmsg.S_State!=1)
+  {
+    if(showDebug)
+      ROS_INFO("Lowest index: %d", current_lowest_index);
 
-	  // If the lowest range is less than 1.5 in length, the robot will begin
-	  // rotating to attempt to avoid the obstacle
-	  if(smallest_range < 1.4) //between 0.8 and 1.5 exclusive
-	  {
-	    // We are getting close to an obstacle, start turning
-	    if(showDebug)
-	      ROS_INFO("Collision at beam: %d | Range: %f", current_lowest_index, smallest_range);
+    // If the lowest range is less than 1.5 in length, the robot will begin
+    // rotating to attempt to avoid the obstacle
+    if(smallest_range < 1.4) //between 0.8 and 1.5 exclusive
+    {
+      // We are getting close to an obstacle, start turning
+      if(showDebug)
+        ROS_INFO("Collision at beam: %d | Range: %f", current_lowest_index, smallest_range);
 
-	    // Slow the robot down to 0.1m/s
-	    RobotNode_cmdvel.linear.x = 0.5;
+      // Slow the robot down to 0.1m/s
+      linear_x = 0.5;
 
-	    // Decide whether to turn anti-clockwise or clockwise depending on which
-	    // side of the robot is closest to the object
-	    if(current_lowest_index < SAMPLE_NUMBER/2) // sample_number is the number of beams
-	    {
-	      RobotNode_cmdvel.angular.z = (M_PI / 18) * 5; //anti-clockwise
-	    }
-	    else
-	    {
-	      RobotNode_cmdvel.angular.z = -(M_PI / 18) * 5; //clockwise
-	    }
-	    if(smallest_range <= 0.8) //we are really close to colliding, stop moving forward
-	    {
-	      RobotNode_cmdvel.linear.x = 0;
-	    }
-	  }
-	  else
-	  {
-	    // If no potential collisions are detected, move forward normally
-	    RobotNode_cmdvel.linear.x = 1;
-	    RobotNode_cmdvel.angular.z = 0;
-	  }
-  }else if(newmsg.S_State==1){
-	ROS_INFO("GOING TO GRASS");
-	RobotNode_cmdvel.linear.x = float(1) * atan2(grassX-px,grassY-py);
-	RobotNode_cmdvel.angular.z = CalculateAnglularVelocity();
-	ROS_INFO("linear velocity: %f   angular velocity: %f", RobotNode_cmdvel.linear.x,RobotNode_cmdvel.angular.z);
+      // Decide whether to turn anti-clockwise or clockwise depending on which
+      // side of the robot is closest to the object
+      if(current_lowest_index < SAMPLE_NUMBER/2) // sample_number is the number of beams
+      {
+        angular_z = (M_PI / 18) * 5; //anti-clockwise
+      }
+      else
+      {
+        angular_z = -(M_PI / 18) * 5; //clockwise
+      }
+      if(smallest_range <= 0.8) //we are really close to colliding, stop moving forward
+      {
+        linear_x = 0;
+      }
+    }
+    else
+    {
+      // If no potential collisions are detected, move forward normally
+      linear_x = 1;
+      angular_z = 0;
+    }
   }
-
+  else if(newmsg.S_State==1)
+  {
+    ROS_INFO("GOING TO GRASS");
+    linear_x = float(1) * atan2(grassX-px,grassY-py);
+    angular_z = CalculateAnglularVelocity();
+    ROS_INFO("linear velocity: %f   angular velocity: %f", linear_x, angular_z);
+  }
 }
 
 float CalculateAnglularVelocity() {
@@ -173,14 +180,14 @@ float CalculateAnglularVelocity() {
 
 	//Find the quadrant to work out difference
 	if (deltaX >= 0 && deltaY >= 0) { // right and up
-		changeInAngle = angle - theta;
+		changeInAngle = angle - ptheta;
 	} else if (deltaX >= 0 && deltaY <= 0) { // right and down
-		changeInAngle = angle - theta;
+		changeInAngle = angle - ptheta;
 	} else if (deltaX <= 0 && deltaY >= 0) { // left and up
-		changeInAngle = 180 - theta + angle;
+		changeInAngle = 180 - ptheta + angle;
 	} else if (deltaX <= 0 && deltaY <= 0) { // left and down
-		changeInAngle = angle - 180 - theta;
-	} 
+		changeInAngle = angle - 180 - ptheta;
+	}
 
 	//Make sure angle is between -360 and 360
 	changeInAngle = fmodf(changeInAngle, 360.0);
@@ -198,17 +205,16 @@ float CalculateAnglularVelocity() {
 		angularZ = -0.5;
 	} else if (changeInAngle <= -180) {
 		angularZ = 0.5;
-	} 
+	}
 	ROS_INFO("grassX: %f   grassY: %f  px:%f   py:%f changeInAngle: %f",grassX,grassY, px, py, changeInAngle);
 	return angularZ;
-	
 }
 
 
 int main(int argc, char** argv){
-  initialPosx = atoi(argv[2]);
-  initialPosy = atoi(argv[3]);
-  initialTheta = atoi(argv[4]);
+  initial_position_x = atoi(argv[2]);
+  initial_position_y = atoi(argv[3]);
+  initial_theta = atoi(argv[4]) / (180/M_PI);
   std::stringstream rName;
   rName.str("");
   rName << "RobotNode" << argv[1];
@@ -221,16 +227,20 @@ int main(int argc, char** argv){
   rName << "robot_" << argv[1]<<"/cmd_vel";
   RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>(rName.str(),1000);
 
-  
+
   rName.str("");
   rName << "robot_" << argv[1]<<"/base_scan";
   ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>(rName.str(),1000,StageLaser_callback);
 
   rName.str("");
-  rName << "robot_" << argv[1]<<"/base_pose_ground_truth";
+  rName << "robot_" << argv[1]<<"/odom";
   ros::Subscriber StageOdom_sub = n.subscribe<nav_msgs::Odometry>(rName.str(),1000,StageOdom_callback);
+  
+  rName.str("");
+  rName << "robot_" << argv[1]<<"/base_pose_ground_truth";
+  ros::Subscriber StageOdom_base_pose_sub = n.subscribe<nav_msgs::Odometry>(rName.str(),1000,StageBasePose_callback);
 
-  //ros::Subscriber grassNode_sub = n.subscribe<alpha_two::grassState>("Grass_msg", 1000, StageGrass_callback); 
+  //ros::Subscriber grassNode_sub = n.subscribe<alpha_two::grassState>("Grass_msg", 1000, StageGrass_callback);
 
   //ros::Publisher sheepNode_state = n.advertise<alpha_two::sheepState>("Sheep_msg",1000);
 
@@ -238,15 +248,19 @@ int main(int argc, char** argv){
   newmsg.S_ID = atoi(argv[1]);
   newmsg.health = 100;
   newmsg.grass_locked = 0;
-  
+
   int count = 0;
   ros::Rate r(10); // 10 cycles per second
 
-  while(n.ok()){
-    ++count;
-   // sheepNode_state.publish(newmsg);
+  while(n.ok())
+  {
+    RobotNode_cmdvel.linear.x = linear_x;
+    RobotNode_cmdvel.angular.z = angular_z;
+    // Publish the message
+    RobotNode_stage_pub.publish(RobotNode_cmdvel);
+    // sheepNode_state.publish(newmsg);
     ros::spinOnce(); //Must Have this statement in the program
     r.sleep();
+    ++count;
   }
 }
-
