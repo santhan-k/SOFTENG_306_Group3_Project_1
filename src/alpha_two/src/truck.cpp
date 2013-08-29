9f4a884d651ac917486093cc20e1340471381efc
Ryun <rmad019@aucklanduni.ac.nz>
2013-08-28 20:07:50 -0700

    Truck will move towards the left of the world.

---
+++ src/alpha_two/src/truck.cpp
@@ -0,0 +1,71 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include "angles/angles.h"
+#include "alpha_two/grassState.h"
+#include "alpha_two/sheepState.h"
+#include "alpha_two/farmState.h"
+#include "alpha_two/rainFall.h"
+#include <sstream>
+#include "math.h"
+
+using namespace std;
+
+alpha_two::farmState new_farm_msg;
+
+int dayCounter;
+double px,py;
+
+
+int velX = 0;
+int velY = 0;
+bool raining;
+
+
+void StageOdom_cloudcallback(nav_msgs::Odometry msg){
+  px = msg.pose.pose.position.x;
+  py = msg.pose.pose.position.y;
+  //printf("HELLO\n");
+}
+
+int main(int argc, char **argv){
+
+  dayCounter = 0;
+  //You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument   is the name of the node
+  ros::init(argc, argv, "Cloud");
+
+  //NodeHandle is the main access point to communicate with ros.
+  ros::NodeHandle n;
+  
+  
+  ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_10/cmd_vel",1000);
+  ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_10/odom",1000, StageOdom_cloudcallback); 
+  ros::Rate loop_rate(10);
+
+
+  //velocity of this RobotNode
+  geometry_msgs::Twist RobotNode_cmdvel;
+  bool reachedLimit = false;
+  while (ros::ok()){
+ 
+  //publish the message
+    if(px<40){
+      RobotNode_cmdvel.linear.x = -1;
+      RobotNode_cmdvel.linear.y = 0;
+    }
+    else{
+      RobotNode_cmdvel.linear.x = 0;
+      RobotNode_cmdvel.linear.y = 0;
+    }
+
+    RobotNode_stage_pub.publish(RobotNode_cmdvel);
+     
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+
+}
