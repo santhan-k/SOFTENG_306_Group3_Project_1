2024d191ea7d54c1eeb55d70238a00e2bfb52ba5
JahnaviB <jbha386@aucklanduni.ac.nz>
2013-08-29 00:02:46 -0700

    Indentation, commenting, removing unused code.

--- src/alpha_two/src/Farmer.cpp
+++ src/alpha_two/src/Farmer.cpp
@@ -31,28 +31,6 @@ struct instruction_struct{
   int next_step; //which step do we move to when we finish this current step
 };
 
-void StageOdom_callback(nav_msgs::Odometry msg){
-  //This is the call back function to process odometry messages coming from Stage.
-  px = 5 + msg.pose.pose.position.x;
-  py =10 + msg.pose.pose.position.y;
-  //ptheta = fmod((2*M_PI) + theta + angles::normalize_angle_positive(asin(msg.pose.pose.orientation.z) * 2), 2*M_PI) * (180/M_PI);
-  ROS_INFO("Current x position is: %f", px);
-  ROS_INFO("Current y position is: %f", py);
-  ROS_INFO("Current theta is: %f", ptheta);
-}
-
-void StageGrass_callback(alpha_two::grassState msg){
- //ROS_INFO("RECEIVED GRASS MESSAGE FROM: %d",msg.G_ID);
-	
-}
-
-//void StageLaser_callback(sensor_msgs::LaserScan msg)
-//{
-//  //This is the callback function to process laser scan messages
-//  //you can access the range data from msg.ranges[i]. i = sample number
-//
-//}
-
 void addInstruction(vector<instruction_struct>& instruction_vector, int step_count, double linear_x, double linear_y, int next_step){
   instruction_struct *Instruction = new instruction_struct; //create a new instruction_struct
   Instruction->step_count = step_count;
@@ -83,15 +61,12 @@ int main(int argc, char **argv){
   //to stage
   ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000);
 
-  //subscribe to listen to messages coming from stage
-  ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, StageOdom_callback);
-  //ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,StageLaser_callback);
-
+  // Subscribes to grass messages
   ros::Subscriber grassNode_sub = n.subscribe<alpha_two::grassState>("Grass_msg", 1000, StageGrass_callback); 
 
   ros::Rate loop_rate(10);
 
-  //a count of howmany messages we have sent
+  //a count of how many messages we have sent
   int count = 0;
 
   fieldNumber = atoi(argv[1]);
@@ -105,22 +80,21 @@ int main(int argc, char **argv){
 
   if(fieldNumber == 1) {
       addInstruction(instruction_vector, 2600, 0.10, -0.10, 1); // Diagonal movement
-      addInstruction(instruction_vector, 78, 0, -0.5, 0);     // Move down
-  } else if(fieldNumber == 2) {
+      addInstruction(instruction_vector, 78, 0, -0.5, 0);       // Move down
+  } 
+  else if(fieldNumber == 2) {
       addInstruction(instruction_vector, 2600, -0.10, -0.10, 1); // Diagonal movement
-      addInstruction(instruction_vector, 75, -0.5, 0, 0);     // Move down
-  } else if(fieldNumber == 3) {
+      addInstruction(instruction_vector, 75, -0.5, 0, 0);        // Move to the left
+  }
+  else if(fieldNumber == 3) {
       addInstruction(instruction_vector, 2600, -0.10, 0.10, 1); // Diagonal movement
-      addInstruction(instruction_vector, 75, 0, 0.5, 0);     // Move down
-  } else if(fieldNumber == 4) {
-      addInstruction(instruction_vector, 2600, 0, 0.10, 1); // Diagonal movement
-      addInstruction(instruction_vector, 75, 0, 0, 0);     // Move down
+      addInstruction(instruction_vector, 75, 0, 0.5, 0);        // Move up
+  }
+  else if(fieldNumber == 4) {
+      addInstruction(instruction_vector, 2600, 0, 0.10, 1);     // Move up
+      addInstruction(instruction_vector, 75, 0, 0, 0);          // Stops
   } 
       
-  //addInstruction(instruction_vector, 75, 0, 0, 0);
-//  addInstruction(instruction_vector, 0, 0, 0.0, 3); // Stop movement
-//  addInstruction(instruction_vector, 75, 1, 0.0, 0); // Outward movement
-
   //keep track of what step we are up to
   int current_step = 0;
   int current_step_count = 0;
@@ -131,16 +105,16 @@ int main(int argc, char **argv){
     linear_y = instruction_vector[current_step].linear_y;
     ++current_step_count;
 
-
+    // Checks to see if the last instruction in the instruction vector has been executed.
     if(instruction_vector[current_step].next_step == 0 && current_step_count == instruction_vector[current_step].step_count) {
-       break;
+       break;   // Stop farmer
     }
 
+    // Goes into next step when the current step finishes.
     if(current_step_count == instruction_vector[current_step].step_count){
       current_step = instruction_vector[current_step].next_step;
       current_step_count = 0;
     }
-
 
     RobotNode_cmdvel.linear.x = linear_x;
     RobotNode_cmdvel.linear.y = linear_y;
@@ -156,10 +130,10 @@ int main(int argc, char **argv){
     ++count;
   }
 
-  RobotNode_cmdvel.linear.x = 0;
-  RobotNode_cmdvel.linear.y =0;
+  RobotNode_cmdvel.linear.x = 0;    // Change velecity back to zero
+  RobotNode_cmdvel.linear.y =0;     // Change velecity back to zero
 
-  RobotNode_stage_pub.publish(RobotNode_cmdvel);
+  RobotNode_stage_pub.publish(RobotNode_cmdvel);    //publish changed velocities
 
   return 0;
 }
