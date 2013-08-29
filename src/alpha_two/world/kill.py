49c94889d8f2a823b225f7813755b49f8fce7751
santhan-k <skus887@aucklanduni.ac.nz>
2013-08-28 17:55:58 -0700

    Now also kills Gates

--- src/alpha_two/world/kill.py
+++ src/alpha_two/world/kill.py
@@ -2,8 +2,9 @@
 from subprocess import Popen, PIPE
 import subprocess
 
-subprocess.Popen("killall R1",shell=True,stdout = PIPE)
-subprocess.Popen("killall R0",shell=True,stdout = PIPE)
-subprocess.Popen("killall stageros",shell=True,stdout = PIPE)
-subprocess.Popen("killall grass",shell=True,stdout = PIPE)
-subprocess.Popen("killall HerdingBar",shell=True,stdout = PIPE)
+subprocess.Popen("killall R1",shell=True,stdout = PIPE, stderr = PIPE)
+subprocess.Popen("killall R0",shell=True,stdout = PIPE, stderr = PIPE)
+subprocess.Popen("killall stageros",shell=True,stdout = PIPE, stderr = PIPE)
+subprocess.Popen("killall grass",shell=True,stdout = PIPE, stderr = PIPE)
+subprocess.Popen("killall HerdingBar",shell=True,stdout = PIPE, stderr = PIPE)
+subprocess.Popen("killall Gates",shell=True,stdout = PIPE, stderr = PIPE)
