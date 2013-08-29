904f61274c317a149ce288813678550f9479ee50
santhan-k <skus887@aucklanduni.ac.nz>
2013-08-29 00:06:41 -0700

    Commented, and removed R1

--- src/alpha_two/world/kill.py
+++ src/alpha_two/world/kill.py
@@ -2,8 +2,8 @@
 from subprocess import Popen, PIPE
 import subprocess
 
+#Kill all processes that are created by pyGen.py
 subprocess.Popen("killall R1",shell=True,stdout = PIPE, stderr = PIPE)
-subprocess.Popen("killall R0",shell=True,stdout = PIPE, stderr = PIPE)
 subprocess.Popen("killall stageros",shell=True,stdout = PIPE, stderr = PIPE)
 subprocess.Popen("killall grass",shell=True,stdout = PIPE, stderr = PIPE)
 subprocess.Popen("killall HerdingBar",shell=True,stdout = PIPE, stderr = PIPE)
